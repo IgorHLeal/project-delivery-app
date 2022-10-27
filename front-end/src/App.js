7d9100937876fe1352e9df82c558e05140e8460c
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-27 19:16:03 -0300

    feat: implementação da route /register do front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
 import Login from './pages/login';
+import Register from './pages/register';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Redirect to="/login" />
         </Route>
         <Route exact path="/login" component={ Login } />
+        <Route exact path="/register" component={ Register } />
       </Switch>
     </BrowserRouter>
   );
