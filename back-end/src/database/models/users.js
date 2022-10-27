39636d31f2ab6956caabd2668c2af7206cee42ad
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-27 17:22:04 -0300

    feat: models de users

---
+++ back-end/src/database/models/users.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+    { timestamps: false })
+
+  // User.associate = (models) => {
+  //   User.hasMany(models.BlogPost,
+  //     { foreignKey: 'id', as: 'userId' });
+  // };
+
+  return User;
+};
