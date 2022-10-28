3168b58e6875826fa0ba9163b4904742ca1be7a8
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-10-28 14:47:44 -0300

    fix: trocado User para users, para dar match com a table

--- back-end/src/database/models/users.js
+++ back-end/src/database/models/users.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('user', {
+  const users = sequelize.define('users', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
@@ -33,5 +33,5 @@ module.exports = (sequelize, DataTypes) => {
   //     { foreignKey: 'id', as: 'userId' });
   // };
 
-  return User;
+  return users;
 };
