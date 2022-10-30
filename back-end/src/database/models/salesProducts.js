9a4f7e66322d6fe911aaced9c9d602c09e1d6f84
Igor Leal <igorhenriqueleal3@gmail.com>
2022-10-30 15:26:12 -0300

    feat: Criando o model de salesProducts

---
+++ back-end/src/database/models/salesProducts.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const salesProducts = sequelize.define('salesProducts', {
+    saleId: {
+      type: DataTypes.INTEGER,
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+    },
+  }, { 
+    timestamps: false,
+    tableName: 'sales_products',
+    modelName: 'salesProducts',
+    underscored: true
+  });
+
+  sales.associate = (models) => {
+    models.sales.belongsToMany(models.products, { 
+      through: salesProducts,
+      as: 'products',
+      foreignKey: 'saleId',
+      otherKey: 'productId',
+    });
+
+    models.products.belongsToMany(models.sales, { 
+      through: salesProducts,
+      as: 'sales',
+      foreignKey: 'productId',
+      otherKey: 'saleId',
+    });
+  };
+
+  return salesProducts;
+};
