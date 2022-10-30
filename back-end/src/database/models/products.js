d85185bb3acbb57b5bd47e75594fa380733cef15
Igor Leal <igorhenriqueleal3@gmail.com>
2022-10-29 23:50:29 -0300

    feat: Criando model, migration e seeder de products

---
+++ back-end/src/database/models/products.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const products = sequelize.define('products', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(4, 2),
+      allowNull: false,
+    },
+    urlImage: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, { 
+    timestamps: false,
+    tableName: 'products',
+    undercored: true
+  })
+
+  return products;
+};
