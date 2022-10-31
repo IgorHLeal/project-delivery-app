2e442ee9f69d17901a19fada69ca17fb5f25e6b1
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-31 20:33:35 -0300

    fix: correções do lint

--- front-end/src/pages/checkout.js
+++ front-end/src/pages/checkout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export default function Checkout() {
   const [products, setProducts] = useState([]);
@@ -13,49 +13,54 @@ export default function Checkout() {
     <div className="checkout">
       {products.length !== 0 ? (
         <ul>
-          {products.map((item, index) =>
-            <li key={item.valor} data-testId={ `element-order-table-item-number${index}` }>
-              <div key={item.name}>
+          {products.map((item, index) => (
+            <li
+              key={ item.valor }
+              data-testId={ `element-order-table-item-number${index}` }
+            >
+              <div key={ item.name }>
                 <p
                   data-testId={ `element-order-table-name-${index}` }
-                  >
+                >
                   {item.name}
                 </p>
                 <p
                   data-testId={ `element-order-table-quantity-${index}` }
-                  >
+                >
                   {item.quantidade}
                 </p>
                 <p
                   data-testId={ `element-order-table-unit-price-${index}` }
-                  >
+                >
                   {item.valor}
                 </p>
                 <p
                   data-testId={ `element-order-table-sub-total-${index}` }
-                  >
+                >
                   {Number(item.quantidade) * Number(item.valor)}
                 </p>
                 <button
-                  onClick={() => removeProduct() }
+                  type="button"
+                  onClick={ () => removeProduct() }
                   data-testId={ `element-order-table-remove-${index}` }
-                  >Remover
+                >
+                  Remover
                 </button>
               </div>
-            </li>
-          )};
-          <h2 data-testId={ `element-order-table-total-price` }></h2>
+            </li>))}
+          ;
+          <h2 data-testId="element-order-table-total-price">TOTAL PRICE</h2>
         </ul>) : null}
 
       <div>
         <select data-testId="customer_checkout__select-seller">
-          {sellers.map((item) => 
-            <options>{ item.name }</options>
-          )};
+          {sellers.map((item, index) => <options key={ index }>{item.name}</options>)}
+          ;
         </select>
         <input data-testId="customer_checkout__input-address" />
         <input type="number" data-testId="customer_checkout__input-address-number" />
         <button
+          type="button"
           data-testId="customer_checkout__button-submit-order"
         >
           Finalizar Pedido
