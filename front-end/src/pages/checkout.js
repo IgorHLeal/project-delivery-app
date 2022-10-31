340d41bf6bc9f12e2a8f7bd074bda9c5fa2586cc
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-10-31 20:11:56 -0300

    feat: criado página de checkouts

---
+++ front-end/src/pages/checkout.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+export default function Checkout() {
+  const [products, setProducts] = useState([]);
+  const [sellers, setSellers] = useState([]);
+
+  const removeProduct = (name) => {
+    setProducts(products.filter((item) => item.name !== name));
+    console.log(setSellers);
+  };
+
+  return (
+    <div className="checkout">
+      {products.length !== 0 ? (
+        <ul>
+          {products.map((item, index) =>
+            <li key={item.valor} data-testId={ `element-order-table-item-number${index}` }>
+              <div key={item.name}>
+                <p
+                  data-testId={ `element-order-table-name-${index}` }
+                  >
+                  {item.name}
+                </p>
+                <p
+                  data-testId={ `element-order-table-quantity-${index}` }
+                  >
+                  {item.quantidade}
+                </p>
+                <p
+                  data-testId={ `element-order-table-unit-price-${index}` }
+                  >
+                  {item.valor}
+                </p>
+                <p
+                  data-testId={ `element-order-table-sub-total-${index}` }
+                  >
+                  {Number(item.quantidade) * Number(item.valor)}
+                </p>
+                <button
+                  onClick={() => removeProduct() }
+                  data-testId={ `element-order-table-remove-${index}` }
+                  >Remover
+                </button>
+              </div>
+            </li>
+          )};
+          <h2 data-testId={ `element-order-table-total-price` }></h2>
+        </ul>) : null}
+
+      <div>
+        <select data-testId="customer_checkout__select-seller">
+          {sellers.map((item) => 
+            <options>{ item.name }</options>
+          )};
+        </select>
+        <input data-testId="customer_checkout__input-address" />
+        <input type="number" data-testId="customer_checkout__input-address-number" />
+        <button
+          data-testId="customer_checkout__button-submit-order"
+        >
+          Finalizar Pedido
+        </button>
+      </div>
+    </div>
+  );
+}
