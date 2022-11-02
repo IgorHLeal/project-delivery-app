54e5c3b0c120062f98da1c8faa31e7c32ce4b1c9
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-11-01 21:31:36 -0300

    refactor: reimplementada a importação da api de products

--- front-end/src/pages/products.js
+++ front-end/src/pages/products.js
@@ -1,11 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import Navbar from '../components/navbar';
 import ProductCard from '../components/product';
 import './products.css';
 import Context from '../context/Context';
 
 export default function Products() {
-  const { products } = useContext(Context);
+  const { token, products, setProducts } = useContext(Context);
+  const history = useHistory();
+
+  useEffect(() => {
+    (async () => {
+      if (!token || token === null) return history.push('/login');
+      const config = { headers: { authorization: token.token } };
+      const { data } = await axios('http://localhost:3001/products', config);
+      setProducts(data);
+    })();
+  }, [token, setProducts, history]);
 
   return (
     <>
