49389ab6f95dc9f4d9c4f83f623adb63064bd195
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-11-01 21:30:17 -0300

    refactor: removida a importação da api de products

--- front-end/src/context/Provider.js
+++ front-end/src/context/Provider.js
@@ -1,28 +1,20 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import Context from './Context';
 import { getLocalStorage } from '../helpers/localStorage';
 
 function Provider({ children }) {
   const [token, setToken] = useState('');
   const [products, setProducts] = useState([]);
-  const history = useHistory();
 
   useEffect(() => {
-    const userData = getLocalStorage('userdata');
+    const userData = getLocalStorage('user');
     setToken(userData);
   }, []);
 
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios('http://localhost:3001/products');
-      setProducts(data);
-    })();
-  }, []);
-
-  const context = useMemo(() => ({ token, products }), [products, token]);
+  const context = useMemo(() => (
+    { token, products, setToken, setProducts }
+  ), [products, token]);
 
   return (
     <div>
