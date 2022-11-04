a69b60d3402978c9762474f12edff134abb91d8e
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-11-04 19:17:12 -0300

    refactor: estado dos cards (ProcuctCard) foram movidos para o componente pai (products)

--- front-end/src/pages/products.js
+++ front-end/src/pages/products.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 // import PropTypes from 'prop-types';
@@ -6,18 +6,28 @@ import Navbar from '../components/navbar';
 import ProductCard from '../components/ProductCard';
 import './products.css';
 import Context from '../context/Context';
-import CartContext from '../context/CartContext';
+// import CartContext from '../context/CartContext';
+// import { setLocalStorage } from '../helpers/localStorage';
 
 export default function Products() {
   const { token, products, setProducts } = useContext(Context);
-  const { cart } = useContext(CartContext);
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
   const history = useHistory();
+  // const { cart } = useContext(CartContext);
+
+  useEffect(() => {
+    const result = cart.reduce((acc, curr) => acc + (curr.quantity * curr.price), 0);
+    setTotalPrice(result);
+  }, [cart]);
 
   useEffect(() => {
     (async () => {
       if (!token || token === null) return history.push('/login');
       const config = { headers: { authorization: token.token } };
       const { data } = await axios('http://localhost:3001/products', config);
+      const modeledData = data.map((item) => ({ ...item, quantity: 0 }));
+      setCart(modeledData);
       setProducts(data);
     })();
   }, [token, setProducts, history]);
@@ -25,18 +35,21 @@ export default function Products() {
   return (
     <>
       <Navbar />
-      {products.map((product) => (
-        <ProductCard key={ product.id } product={ product } />))}
-      {cart.length !== 0
-        ? (
-          <button
-            type="button"
-          >
-            Ver carrinho:
+      {products.map((product, index) => (
+        <ProductCard
+          key={ product.id }
+          object={ { product, index, cart, setCart } }
+        />))}
+      <button
+        type="button"
+        onClick={ () => { history.push('/customer/checkout'); } }
+      >
+        <div>Ver carrinho:</div>
+        <div data-testid="customer_products__checkout-bottom-value">
+          {totalPrice.toFixed(2).replace('.', ',')}
+        </div>
+      </button>
 
-          </button>
-        )
-        : ''}
     </>
   );
 }
